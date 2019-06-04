7b5ac5e759d13984df6aaef93feedd41bfbacded
juanchaco677 <31424234+juanchaco677@users.noreply.github.com>
2019-06-04 13:58:29 +0000

    animacion profile probar

--- lib/resource/home/pages/ProfilePages.dart
+++ lib/resource/home/pages/ProfilePages.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePages extends StatefulWidget {
@@ -23,6 +24,22 @@ class _ProfilePagesState extends State<ProfilePages>
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AvatarGlow(
+    glowColor: Colors.blue,
+    endRadius: 90.0,
+    duration: Duration(milliseconds: 2000),
+    repeat: true,
+    showTwoGlows: true,
+    repeatPauseDuration: Duration(milliseconds: 100),
+    child: Material(
+      elevation: 8.0,
+      shape: CircleBorder(),
+      child: CircleAvatar(
+        backgroundColor:Colors.grey[100] ,
+        child: Image.asset('assets/images/flutter.png',height: 60,),
+        radius: 40.0,
+      ),
+    ),
+  );
   }
 }
